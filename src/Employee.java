128bd687af8b32a47ddb624f60e3e9c3de060f3b
JNBG <jneuerburg@aol.com>
2017-01-04 15:02:46 +0100

    added employee class

---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+import java.lang.reflect.Array;
+
+/**
+ * Created by Joscha on 04.01.2017.
+ */
+public class Employee {
+    //Constructor
+    public Employee() {
+    }
+
+    //Attributes
+    public String name;
+    public String lastName;
+    public String status:
+    public String role;
+    public String agency;
+    public int empID;
+    //TODO: private CustomerPerson currCustPers
+    //TODO: private CustomerEnterprise currCustEnt
+
+    //Methods
+    //add Customer to employee
+    //TODO: Needs Customer-Attributes from Customer Class
+    public Boolean newCustomer (){
+        return true;
+    }
+
+    //edit a Customer of an employee
+    //TODO: create method
+    public Boolean editCustomer (){
+        return true;
+    }
+
+    //add new Search for objects associated with Employee
+    //TODO: create method
+    public Boolean newSearch(){
+        return true;
+    }
+
+    //Acquires corresponding Customer-Person of Employee
+    //TODO: create method
+    public Boolean GetCurrCustPerson (){
+        return true;
+    }
+
+    //Acquires corresponding Customer-Enterprise of Employee
+    //TODO: create method
+    public Boolean GetCurrCustEnt (){
+        return true;
+    }
+}
