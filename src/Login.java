d7447b0f178cbb2575abaca7adf64b65e32f890d
Philipp <Philipp.Kirste@gmx.de>
2017-01-04 14:52:59 +0100

    Creating Login Class

--- src/Login.java
+++ src/Login.java
@@ -2,4 +2,46 @@
  * Created by Philipp on 04.01.2017.
  */
 public class Login {
+
+    private Boolean isLoggedIn = false;
+    // TODO Login.LogoutTimer: private int logoutTimer
+    // TODO Login.Employee: public Employee currEmployee
+
+
+
+    // CONSTRUCTOR
+    public Login() {
+    }
+
+
+
+    // METHODS:
+    // PUBLIC:
+
+    //TODO Login.login()
+    // used to login an Employee, gets username and password and checks the login
+    public Boolean login(){
+        return true;
+    }
+
+    //TODO Login.logout()
+    public Boolean logout(){
+        return true;
+    }
+
+
+
+    // PRIVATE:
+
+    //TODO Login.checkLogin()
+    //checks in Database if entered username and password are correct
+    private Boolean checkLogin(){
+        return true;
+    }
+
+    //TODO Login.AutoLogout()
+    private void autoLogout(){
+
+    }
+
 }
