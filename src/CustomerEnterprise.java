c34b1339b933f136c0bb9435b06a803728ccbf0a
Philipp <Philipp.Kirste@gmx.de>
2017-01-04 16:25:05 +0100

    Creating Customer Child Classes

---
+++ src/CustomerEnterprise.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+
+/**
+ * Created by Philipp on 04.01.2017.
+ */
+public class CustomerEnterprise extends Customer{
+    public CustomerEnterprise() {
+    }
+
+    public String enterpriseName;
+    public Calendar foundingDate;
+    public int annualRevenue;
+    public int employeeCount;
+    public String typeOfEnterprise;
+}
---
+++ src/CustomerPerson.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+
+/**
+ * Created by Philipp on 04.01.2017.
+ */
+public class CustomerPerson extends Customer{
+    public CustomerPerson() {
+    }
+
+    public String name;
+    public String lastName;
+    public Calendar birthdate;
+    public String relationshipStatus;
+}
