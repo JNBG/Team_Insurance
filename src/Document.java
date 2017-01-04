cfdf4f5d75491cdaa734c019e89373f757e76e68
Philipp <Philipp.Kirste@gmx.de>
2017-01-04 16:35:48 +0100

    Creating Damage and Document Classes

---
+++ src/Document.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Philipp on 04.01.2017.
+ */
+public class Document {
+    public Document() {
+    }
+
+    public String name;
+    public String type;             // .txt .pdf .odt ...
+    public int documentID;          // unique ID of the Document, also stored in Database
+}
