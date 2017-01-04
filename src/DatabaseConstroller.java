82b7e606d15bdcc922ff35a5f68a3b7e9e3982ef
Philipp <Philipp.Kirste@gmx.de>
2017-01-04 16:49:35 +0100

    Creating Search and DatabaseController Classes

---
+++ src/DatabaseConstroller.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Philipp on 04.01.2017.
+ */
+public class DatabaseConstroller {
+    public DatabaseConstroller() {
+    }
+
+    private Login resultLogin;
+    private Employee resultEmployee;
+    private Customer[] resultsCustomer;
+    private Insurance[] resultsInsurance;
+    private Document[] resultsDocument;
+    private Damage[] resultsDamage;
+
+    public boolean searchLogin(){
+        return true;
+    }
+
+    public Employee searchEmployee(){
+
+    }
+
+    public Customer[] searchCustomer(){
+
+    }
+
+    public Insurance[] searchInsurance(){
+
+    }
+
+    public Document[] searchDokument(){
+
+    }
+
+    public Damage[] searchDamage(){
+
+    }
+
+}
