e09c74a308c5380f7f938a12147be71b2b2ac862
JNBG <jneuerburg@aol.com>
2017-01-18 19:39:08 +0100

    added two Tests for login()

--- tests/login/LoginTest.java
+++ tests/login/LoginTest.java
@@ -20,5 +20,20 @@ public class LoginTest {
         assertFalse(Paul.login());
     }
 
+    /* ###ZU TRIVIAL?###
+    @Test
+    public void loginCheck() throws Exception {
+        Login Max = new Login("Max", "whatevs");
+        Max.login();
+        assertTrue(Max.getIsLoggedIn());
+    }
+    */
 
+    @Test
+    public void logout () throws Exception {
+        Login Max = new Login("Max", "whatevs");
+        Max.login();
+        Max.logout();
+        assertFalse(Max.getIsLoggedIn());
+    }
 }
