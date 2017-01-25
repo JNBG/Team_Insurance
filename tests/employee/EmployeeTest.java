8af4a18817c4c335d6da7c310c63d56a3263bccc
JNBG <jneuerburg@aol.com>
2017-01-25 18:04:23 +0100

    changing Test for Employee

--- tests/employee/EmployeeTest.java
+++ tests/employee/EmployeeTest.java
@@ -1,5 +1,6 @@
 package employee;
 
+import customer.CustomerPerson;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,12 +8,14 @@ import static org.junit.Assert.*;
 public class EmployeeTest {
     @Test
     public void newCustomerPerson() throws Exception {
-
-    }
+        Employee PersontTester = new Employee();
+        assertTrue(PersontTester.newCustomerPerson("Alex", "Werauchimmer", "06.12.1974", "married", "München", 98746, "Poststraße", 7, "Phone: 09874/45219", "v", "vn, vp, bz"));
+        }
 
     @Test
     public void newCustomerEnterprise() throws Exception {
-
+        Employee EnterpriseTester = new Employee();
+        assertTrue(EnterpriseTester.newCustomerEnterprise("Microsoft","01.02.1970", 245011288.0, 876442, "Inc.", "Mountain View", 87962, "California Street", 45, "Phone: 87965/454213", "p", "vn, vp, bz"));
     }
 
     @Test
@@ -22,16 +25,6 @@ public class EmployeeTest {
 
     @Test
     public void searchCustomer() throws Exception {
-
-    }
-
-    @Test
-    public void getCurrCustPerson() throws Exception {
-
-    }
-
-    @Test
-    public void getCurrCustEnt() throws Exception {
 
     }
 
