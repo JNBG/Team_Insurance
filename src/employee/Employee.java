62b393878878cb058b9a5d97200c9f8bbcfeba86
Philipp <Philipp.Kirste@gmx.de>
2017-01-25 15:56:44 +0100

    - changing CustomerEnterprise Constructor

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -34,20 +34,42 @@ public class Employee {
 
     /**
      * Creates a new Customer with given information
-     * @param cName
-     * @param cLastName
-     * @param cBirthday (Birthday in format 'dd.mm.yyyy')
-     * @param cRelationshipStatus (married, single,...)
-     * @param cCity
-     * @param cZip
-     * @param cCommunication (ways to contact the Customer)
-     * @param cCustomerClass (v=vip, a=better/profit>3500, b=normal/profit>1000, p=potential customer, z=not worthy)
-     * @param cCustomerType (VN=Versicherungsnehmer, VP=versicherte Person, BZ=Beitragszahler)
+     * @param cName Firstname of the new Customer
+     * @param cLastName Lastname of the new Customer
+     * @param cBirthday Birthday in format 'dd.mm.yyyy'
+     * @param cRelationshipStatus married, single,...
+     * @param cCity city where currently registered
+     * @param cZip zip of current city
+     * @param cCommunication ways to contact the Customer
+     * @param cCustomerClass v=vip, a=better/profit>3500, b=normal/profit>1000, p=potential customer, z=not worthy
+     * @param cCustomerType VN=Versicherungsnehmer, VP=versicherte Person, BZ=Beitragszahler
      * @return bool if creation was successful
      */
     public Boolean newCustomerPerson (String cName, String cLastName, String cBirthday, String cRelationshipStatus, String cCity, int cZip, String cStreetName, int cHouseNumber, String cCommunication, String cCustomerClass, String cCustomerType){
-        CustomerPerson newCustomerPerson = new CustomerPerson(cName, cLastName, cBirthday, cRelationshipStatus, cCity, cZip, cStreetName, cHouseNumber ,cCommunication, cCustomerClass, cCustomerType, this.empID);
-        this.currCustPerson = newCustomerPerson;
+        CustomerPerson newCP = new CustomerPerson(cName, cLastName, cBirthday, cRelationshipStatus, cCity, cZip, cStreetName, cHouseNumber ,cCommunication, cCustomerClass, cCustomerType, this.empID);
+        this.currCustPerson = newCP;
+        this.currCustEnterprise = null;
+        return true;
+    }
+
+    /**
+     *
+     * @param cEnterpriseName String: Name of the Enterprise
+     * @param cFoundingDate String: Founding Date ('dd.mm.yyyy')
+     * @param cAnnualRevenue int in €
+     * @param cEmployeeCount int
+     * @param cTypeOfEnterprise duno
+     * @param cCity city where currently registered
+     * @param cZip zip of current city
+     * @param cCommunication ways to contact the Customer
+     * @param cCustomerClass v=vip, a=better/profit>3500, b=normal/profit>1000, p=potential customer, z=not worthy
+     * @param cCustomerType VN=Versicherungsnehmer, VP=versicherte Person, BZ=Beitragszahler
+     * @return bool if creation was successful
+     */
+    public Boolean newCustomerEnterprise (String cEnterpriseName, String cFoundingDate, int cAnnualRevenue, int cEmployeeCount, String cTypeOfEnterprise, String cCity, int cZip, String cStreetName, int cHouseNumber, String cCommunication, String cCustomerClass, String cCustomerType){
+        CustomerEnterprise newCE = new CustomerEnterprise(cEnterpriseName, cFoundingDate, cAnnualRevenue, cEmployeeCount, cTypeOfEnterprise, cCity, cZip, cStreetName, cHouseNumber ,cCommunication, cCustomerClass, cCustomerType, this.empID);
+        this.currCustEnterprise = newCE;
+        this.currCustPerson = null;
         return true;
     }
 
