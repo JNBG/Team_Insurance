cc205246562962b9628198f07e15641d40503c74
Philipp <Philipp.Kirste@gmx.de>
2017-01-26 08:31:43 +0100

    - changing Search.searchCustomer

--- src/search/Search.java
+++ src/search/Search.java
@@ -15,62 +15,62 @@ public class Search {
     private CustomerPerson[] resultsCustomerPerson;
     private CustomerEnterprise[] resultsCustomerEnterprise;
 
-    public CustomerPerson[] searchCustomer(String keyword){
+    public boolean searchCustomer(String keyword){
         DatabaseController CustPersonDB = new DatabaseController();
         CustomerPerson[] checkPerson = CustPersonDB.searchCustomerPerson();
         int personSize = checkPerson.length;
 
         List<CustomerPerson> searchPersonResults = new ArrayList<CustomerPerson>();
 
-        for (int arrayPos = 0; arrayPos <= personSize; arrayPos++) {
+        for (int arrayPos = 0; arrayPos < personSize; arrayPos++) {
             if (checkPerson[arrayPos].name.equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
             } else if (checkPerson[arrayPos].lastName.equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
             } else if (checkPerson[arrayPos].city.equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
-            } else if (checkPerson[arrayPos].zip == Integer.parseInt(keyword)) {
+            } else if (String.valueOf(checkPerson[arrayPos].zip).equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
             } else if (checkPerson[arrayPos].streetName.equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
-            }  else if (checkPerson[arrayPos].houseNumber == Integer.parseInt(keyword)) {
+            }  else if (String.valueOf(checkPerson[arrayPos].houseNumber).equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
             } else if (checkPerson[arrayPos].customerClass.equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
-            } else if (checkPerson[arrayPos].consultant == Integer.parseInt(keyword)) {
+            } else if (String.valueOf(checkPerson[arrayPos].consultant).equals(keyword)) {
                 searchPersonResults.add(checkPerson[arrayPos]);
             }
-            searchPersonResults.toArray(resultsCustomerPerson);
         }
-
+        resultsCustomerPerson = new CustomerPerson[searchPersonResults.size()];
+        resultsCustomerPerson = searchPersonResults.toArray(resultsCustomerPerson);
 
         CustomerEnterprise[] checkEnterprise = CustPersonDB.searchCustomerEnterprise();
         int entSize = checkEnterprise.length;
 
         List<CustomerEnterprise> searchEnterpriseResults = new ArrayList<CustomerEnterprise>();
 
-        for (int arrayPos = 0; arrayPos <= entSize; arrayPos++) {
+        for (int arrayPos = 0; arrayPos < entSize; arrayPos++) {
             if (checkEnterprise[arrayPos].enterpriseName.equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
             } else if (checkEnterprise[arrayPos].typeOfEnterprise.equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
             } else if (checkEnterprise[arrayPos].city.equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
-            } else if (checkEnterprise[arrayPos].zip == Integer.parseInt(keyword)) {
+            } else if (String.valueOf(checkEnterprise[arrayPos].zip).equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
             } else if (checkEnterprise[arrayPos].streetName.equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
-            }  else if (checkEnterprise[arrayPos].houseNumber == Integer.parseInt(keyword)) {
+            }  else if (String.valueOf(checkEnterprise[arrayPos].houseNumber).equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
             } else if (checkEnterprise[arrayPos].customerClass.equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
-            } else if (checkEnterprise[arrayPos].consultant == Integer.parseInt(keyword)) {
+            } else if (String.valueOf(checkEnterprise[arrayPos].consultant).equals(keyword)) {
                 searchEnterpriseResults.add(checkEnterprise[arrayPos]);
             }
-            searchEnterpriseResults.toArray(resultsCustomerEnterprise);
         }
-
-        return null;
+        resultsCustomerEnterprise = new CustomerEnterprise[searchEnterpriseResults.size()];
+        resultsCustomerEnterprise = searchEnterpriseResults.toArray(resultsCustomerEnterprise);
+        return true;
     }
 
     public CustomerPerson[] getResultsCustomerPerson() {
--- tests/employee/EmployeeTest.java
+++ tests/employee/EmployeeTest.java
@@ -25,8 +25,8 @@ public class EmployeeTest {
 
     @Test
     public void searchCustomer() throws Exception {
-        Employee Birgit = new Employee();
-        assertTrue(Birgit.searchCustomer("Erfurt"));
+        Employee SearchTester = new Employee();
+        assertTrue(SearchTester.searchCustomer("v"));
 
     }
 
