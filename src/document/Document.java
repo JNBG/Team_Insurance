eb3080e0a5005aced3aeedce902c2415ef03c611
Philipp <Philipp.Kirste@gmx.de>
2017-02-06 17:44:35 +0100

    changing Document class

--- src/document/Document.java
+++ src/document/Document.java
@@ -7,7 +7,25 @@ public class Document {
     public Document() {
     }
 
+    public Document(String dName, String dType, int dDocumentID) {
+        this.name = dName;
+        this.name = dType;
+        this.documentID = dDocumentID;
+    }
+
     public String name;
     public String type;             // .txt .pdf .odt ...
     public int documentID;          // unique ID of the document.document, also stored in Database
+
+
+    public boolean changeName(String newName){
+        this.name = newName;
+        // save change in DB
+        // if (successfull){
+            return true;
+        // } else {
+        //  return false;
+        // }
+    }
+
 }
